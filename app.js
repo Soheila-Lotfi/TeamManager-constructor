ccd31299c1542d35bb4228c4c9c2a1abf89aa4f7
soheila <soheilalotfi@ymail.com>
2019-07-27 11:46:12 -0400

    create recoercion to create 3 unique players

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ this.position=position;
 this.offense=offense;
 this.defense=defense;
 this.goodGame=function(){
- if((Math.floor(Math.random()*2)===0){
+ if((Math.floor(Math.random()*2))===0){
      this.defense++;
  }
  else{
@@ -28,6 +28,7 @@ var inquirer=require('inquirer');
 
 var count=0;
 var playerArray=[];
+
 var askQuestion= function(){
 
     if(count<4){
@@ -46,9 +47,12 @@ var askQuestion= function(){
             message:"how good is your defense?"   
                 }
             ]).then(function(answers){
-                var firstStarter=new Player(answrs.name,answers.position,answers.offense,answers.defense);
-    
+                var newPlayer=new Player(answrs.name,answers.position,answers.offense,answers.defense);
+                  playerArray.push(newPlayer);
+                  count++;
+                  askQuestion();
             })
+            
     }
 
 }
